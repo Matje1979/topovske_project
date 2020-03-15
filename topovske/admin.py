4021fb05817dd935100ed349824fd34a29c02890
MaliRobot <milos.trifunovic@gmail.com>
2020-03-15 21:19:38 +0100

    update settings

--- topovske/admin.py
+++ topovske/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Index, Project, Camp, Archive, Victim, Photo, Interview
+
+admin.site.register(Index)
+admin.site.register(Project)
+admin.site.register(Camp)
+admin.site.register(Archive)
+admin.site.register(Victim)
+admin.site.register(Photo)
+admin.site.register(Interview)
